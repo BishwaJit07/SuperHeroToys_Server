15c8cac750d9b1d1d397e27b60c8d28b1826e2ae
BishwaJit07 <bishwajitr50@gmail.com>
2023-05-19 23:36:33 +0600

    vercel

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 const app = express();
 require('dotenv').config()
 const port = process.env.PORT || 5000;
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 //middleware
 app.use(cors());
@@ -25,7 +25,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
 
     const toysCollection = client.db('SuperHeroToys').collection('ToysCollection');
 
@@ -34,9 +34,21 @@ async function run() {
       const result = await cursor.toArray();
       res.send(result);
     })
+
+    app.get('/toys/:id', async(req,res)=>{
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id)}
+      
+    const options = {
+      projection: { picture_url: 1,  name : 1, sub_category:1,rating : 1, available_quantity:1,description:1, price: 1 ,},
+    };
+      const result = await toysCollection.findOne(query,options);
+      res.send(result);
+   })
+
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    // await client.db("admin").command({ ping: 1 });
+    // console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
